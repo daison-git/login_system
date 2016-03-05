ad7a79b3b99c35cc92fa035160697564b86a02e7
shukatsudaison@gmail.com <vagrant@localhost.localdomain>
2016-03-05 23:20:58 +0900

    ログインユーザーの表示

--- index.php
+++ index.php
@@ -1,12 +1,25 @@
 <?php
 
+require_once('config.php');
+require_once('functions.php');
+
 session_start();
 
-if (empty($_SESSION['id']))
-{
+if (empty($_SESSION['id'])) {
     header('Location: login.php');
     exit;
 }
+
+$dbh = connectDatabase();
+
+$sql = "select * from users where id = :id";
+$stmt = $dbh->prepare($sql);
+
+$stmt->bindParam(":id", $_SESSION['id']);
+
+$stmt->execute();
+
+$user = $stmt->fetch();
 
 ?>
 <!DOCTYPE html>
@@ -17,5 +30,6 @@ if (empty($_SESSION['id']))
 </head>
 <body>
     <h1>登録したユーザーのみ閲覧可能です！</h1>
+    <h2><?php echo h($user['name']) ?>さん ようこそ！</h2>
 </body>
 </html>
