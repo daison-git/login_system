b290dd78041c57fa00146d69045101bc242538ab
shukatsudaison@gmail.com <vagrant@localhost.localdomain>
2016-03-05 22:43:49 +0900

    新規ユーザー登録フォームの作成

---
+++ signup.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>新規登録画面</title>
+</head>
+<body>
+    <h1>新規ユーザー登録</h1>
+    <form action="" method="post">
+        ユーザネーム: <input type="text" name="name"><br>
+        パスワード: <input type="text" name="password"><br>
+        <input type="submit" value="新規登録">
+    </form>
+    <a href="login.php">ログインはこちら</a>
+</body>
+</html>
