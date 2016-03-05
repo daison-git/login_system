19b194ecbaf3141b6b626dde21e3c6ad7dd41b3d
shukatsudaison@gmail.com <vagrant@localhost.localdomain>
2016-03-05 22:55:36 +0900

    新規登録のバリデーション

--- signup.php
+++ signup.php
@@ -1,11 +1,47 @@
+<?php
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST')
+{
+    $name = $_POST['name'];
+    $password = $_POST['password'];
+
+    $errors = array();
+
+    // バリデーション
+    if ($name == '') {
+        $errors[] = 'ユーザネームが未入力です';
+    }
+
+    if ($password == '') {
+        $errors[] = 'パスワードが未入力です';
+    }
+
+}
+
+?>
 <!DOCTYPE html>
 <html lang="ja">
 <head>
     <meta charset="UTF-8">
     <title>新規登録画面</title>
 </head>
+<style>
+.error {
+    color: red;
+    list-style: none;
+}
+</style>
 <body>
     <h1>新規ユーザー登録</h1>
+
+    <?php if (isset($errors)) : ?>
+        <div class="error">
+            <?php foreach ($errors as $error) : ?>
+                <li><?php echo $error; ?></li>
+            <?php endforeach; ?>
+        </div>
+    <?php endif; ?>
+
     <form action="" method="post">
         ユーザネーム: <input type="text" name="name"><br>
         パスワード: <input type="text" name="password"><br>
